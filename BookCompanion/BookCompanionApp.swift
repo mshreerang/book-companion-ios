2869bed0f6a4e250a097fe6e4f2f489b54089c4d
mshreerang <shree.mandlekar@gmail.com>
2026-01-18 22:23:03 +0000

    Initial Commit

---
+++ BookCompanion/BookCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCompanionApp.swift
+//  BookCompanion
+//
+//  Created by Shree on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookCompanionTests/BookCompanionTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookCompanionTests.swift
+//  BookCompanionTests
+//
+//  Created by Shree on 18/01/2026.
+//
+
+import Testing
+
+struct BookCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
